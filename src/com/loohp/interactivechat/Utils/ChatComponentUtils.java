bfd5a1ea135f0a217daef5cdc65854715a636f51
LOOHP <jamesloohp@gmail.com>
2020-07-05 22:08:08 +0800

    Update ChatComponentUtils.java

--- src/com/loohp/interactivechat/Utils/ChatComponentUtils.java
+++ src/com/loohp/interactivechat/Utils/ChatComponentUtils.java
@@ -73,12 +73,14 @@ public class ChatComponentUtils {
 		if (base1.isUnderlined() != base2.isUnderlined()) {
 			return false;
 		}
-		if ((base1.getFontRaw() == null && base2.getFontRaw() != null) || (base1.getFontRaw() != null && base2.getFontRaw() == null)) {
-			return false;
-		}
-		if (base1.getFontRaw() != null && base2.getFontRaw() != null) {
-			if (!base1.getFontRaw().equals(base2.getFontRaw())) {
+		if (InteractiveChat.version.isPost1_16()) {
+			if ((base1.getFontRaw() == null && base2.getFontRaw() != null) || (base1.getFontRaw() != null && base2.getFontRaw() == null)) {
 				return false;
+			}
+			if (base1.getFontRaw() != null && base2.getFontRaw() != null) {
+				if (!base1.getFontRaw().equals(base2.getFontRaw())) {
+					return false;
+				}
 			}
 		}
 		if (compareText && !base1.toLegacyText().equals(base2.toLegacyText())) {
@@ -111,12 +113,14 @@ public class ChatComponentUtils {
 		if (base1.isUnderlined() != base2.isUnderlined()) {
 			return false;
 		}
-		if ((base1.getFontRaw() == null && base2.getFontRaw() != null) || (base1.getFontRaw() != null && base2.getFontRaw() == null)) {
-			return false;
-		}
-		if (base1.getFontRaw() != null && base2.getFontRaw() != null) {
-			if (!base1.getFontRaw().equals(base2.getFontRaw())) {
+		if (InteractiveChat.version.isPost1_16()) {
+			if ((base1.getFontRaw() == null && base2.getFontRaw() != null) || (base1.getFontRaw() != null && base2.getFontRaw() == null)) {
 				return false;
+			}
+			if (base1.getFontRaw() != null && base2.getFontRaw() != null) {
+				if (!base1.getFontRaw().equals(base2.getFontRaw())) {
+					return false;
+				}
 			}
 		}
 		if (compareText && !base1.toLegacyText().equals(base2.toLegacyText())) {

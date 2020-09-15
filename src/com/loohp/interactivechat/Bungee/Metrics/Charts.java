42c9b3ce6549d0d56f390399d801d985f1453cbe
LOOHP <jamesloohp@gmail.com>
2020-09-15 11:56:41 +0800

    Fixed typo in metrics

--- src/com/loohp/interactivechat/Bungee/Metrics/Charts.java
+++ src/com/loohp/interactivechat/Bungee/Metrics/Charts.java
@@ -8,7 +8,7 @@ public class Charts {
 	
 	public static void setup(Metrics metrics) {
 		
-		metrics.addCustomChart(new Metrics.SingleLineChart("total_plugin_messages_relayed_per_interval)", new Callable<Integer>() {
+		metrics.addCustomChart(new Metrics.SingleLineChart("total_plugin_messages_relayed_per_interval", new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
             	long amount = InteractiveChatBungee.pluginMessagesCounter.getAndSet(0);

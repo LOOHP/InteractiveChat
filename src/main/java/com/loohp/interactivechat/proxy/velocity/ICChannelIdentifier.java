a1cdb65e66558321e828bf82acc171f322bffe12
LOOHP <jamesloohp@gmail.com>
2021-04-06 21:15:37 +0800

    clear

--- src/main/java/com/loohp/interactivechat/proxy/velocity/ICChannelIdentifier.java
+++
@@ -1,9 +0,0 @@
-package com.loohp.interactivechat.proxy.velocity;
-
-import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
-
-public class ICChannelIdentifier {
-	
-	public static final MinecraftChannelIdentifier INSTANCE = MinecraftChannelIdentifier.from("interchat:main");
-
-}

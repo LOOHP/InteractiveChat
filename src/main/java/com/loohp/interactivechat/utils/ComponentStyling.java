3a8c17cf3b428610675831f037dd2193007c1f3d
LOOHP <jamesloohp@gmail.com>
2021-06-11 02:58:04 +0800

    Added splitAtLineBreaks method for use in InteractiveChatDiscordSRVAddon

--- src/main/java/com/loohp/interactivechat/utils/ComponentStyling.java
+++ src/main/java/com/loohp/interactivechat/utils/ComponentStyling.java
@@ -1,9 +1,11 @@
 package com.loohp.interactivechat.utils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.TextColor;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
@@ -18,6 +20,35 @@ public class ComponentStyling {
 			children.set(i, stripColor(children.get(i)));
 		}
 		return component.children(children);
+	}
+	
+	public static List<Component> splitAtLineBreaks(Component component) {
+		List<Component> filtered = new ArrayList<>();
+		component = ComponentFlattening.flatten(component);
+		List<Component> currentChildrens = new ArrayList<>();
+		for (Component child : component.children()) {
+			if (child instanceof TextComponent) {
+				TextComponent textComponent = (TextComponent) child;
+				String[] parts = textComponent.content().split("\n", -1);
+				if (parts.length > 1) {
+					currentChildrens.add(textComponent.content(parts[0]));
+					filtered.add(ComponentCompacting.optimize(component.children(currentChildrens)));
+					for (int i = 1; i < parts.length - 1; i++) {
+						filtered.add(ComponentCompacting.optimize(component.children(Collections.emptyList()).append(textComponent.content(parts[i]))));
+					}
+					currentChildrens = new ArrayList<>();
+					currentChildrens.add(textComponent.content(parts[parts.length - 1]));
+				} else {
+					currentChildrens.add(child);
+				}
+			} else {
+				currentChildrens.add(child);
+			}
+		}
+		if (currentChildrens.size() > 0) {
+			filtered.add(ComponentCompacting.optimize(component.children(currentChildrens)));
+		}
+		return filtered;
 	}
 	
 	public static TextColor getFirstColor(Component component) {

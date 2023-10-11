34b8e4f46a4cede7a899a2c89c87ece300bc3fd5
LOOHP <jamesloohp@gmail.com>
2023-10-11 15:16:11 +0100

    Fixed toast in 1.20.2

--- src/main/java/com/loohp/interactivechat/utils/ToastUtils.java
+++ src/main/java/com/loohp/interactivechat/utils/ToastUtils.java
@@ -40,6 +40,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class ToastUtils {
@@ -58,8 +59,12 @@ public class ToastUtils {
     private static Class<?> nmsCriterionClass;
     private static Class<?> nmsCriterionTriggerImpossibleAClass;
     private static Constructor<?> nmsCriterionTriggerImpossibleAConstructor;
+    private static Class<?> nmsCriterionTriggerImpossibleClass;
+    private static Constructor<?> nmsCriterionTriggerImpossibleConstructor;
     private static Class<?> nmsCriterionInstanceClass;
     private static Constructor<?> nmsCriterionConstructor;
+    private static Class<?> nmsAdvancementRequirementsClass;
+    private static Constructor<?> nmsAdvancementRequirementsConstructor;
     private static Class<?> nmsAdvancementClass;
     private static Constructor<?> nmsAdvancementConstructor;
     private static Class<?> nmsAdvancementProgressClass;
@@ -69,6 +74,8 @@ public class ToastUtils {
     private static Method nmsAdvancementProgressGetCriterionProgressBMethod;
     private static Class<?> nmsPacketPlayOutAdvancementsClass;
     private static Constructor<?> nmsPacketPlayOutAdvancementsConstructor;
+    private static Class<?> nmsAdvancementHolderClass;
+    private static Constructor<?> nmsAdvancementHolderConstructor;
 
     static {
         try {
@@ -86,17 +93,31 @@ public class ToastUtils {
             nmsCriterionClass = NMSUtils.getNMSClass("net.minecraft.server.%s.Criterion", "net.minecraft.advancements.Criterion");
             nmsCriterionTriggerImpossibleAClass = NMSUtils.getNMSClass("net.minecraft.server.%s.CriterionTriggerImpossible$a", "net.minecraft.advancements.critereon.CriterionTriggerImpossible$a");
             nmsCriterionTriggerImpossibleAConstructor = nmsCriterionTriggerImpossibleAClass.getConstructor();
-            nmsCriterionInstanceClass = NMSUtils.getNMSClass("net.minecraft.server.%s.CriterionInstance", "net.minecraft.advancements.CriterionInstance");
-            nmsCriterionConstructor = nmsCriterionClass.getConstructor(nmsCriterionInstanceClass);
+            if (InteractiveChat.version.isNewerOrEqualTo(MCVersion.V1_20_2)) {
+                nmsCriterionTriggerImpossibleClass = NMSUtils.getNMSClass("net.minecraft.server.%s.CriterionTriggerImpossible", "net.minecraft.advancements.critereon.CriterionTriggerImpossible");
+                nmsCriterionTriggerImpossibleConstructor = nmsCriterionTriggerImpossibleClass.getConstructor();
+                nmsCriterionConstructor = Arrays.stream(nmsCriterionClass.getConstructors()).filter(c -> c.getParameterCount() == 2).findFirst().get();
+            } else {
+                nmsCriterionInstanceClass = NMSUtils.getNMSClass("net.minecraft.server.%s.CriterionInstance", "net.minecraft.advancements.CriterionInstance");
+                nmsCriterionConstructor = nmsCriterionClass.getConstructor(nmsCriterionInstanceClass);
+            }
             nmsAdvancementClass = NMSUtils.getNMSClass("net.minecraft.server.%s.Advancement", "net.minecraft.advancements.Advancement");
             nmsAdvancementConstructor = NMSUtils.reflectiveLookup(Constructor.class, () -> {
                 return nmsAdvancementClass.getConstructor(nmsMinecraftKeyClass, nmsAdvancementClass, nmsAdvancementDisplayClass, nmsAdvancementRewardsClass, Map.class, String[][].class);
             }, () -> {
                 return nmsAdvancementClass.getConstructor(nmsMinecraftKeyClass, nmsAdvancementClass, nmsAdvancementDisplayClass, nmsAdvancementRewardsClass, Map.class, String[][].class, boolean.class);
+            }, () -> {
+                nmsAdvancementRequirementsClass = NMSUtils.getNMSClass("net.minecraft.server.%s.AdvancementRequirements", "net.minecraft.advancements.AdvancementRequirements");
+                nmsAdvancementRequirementsConstructor = nmsAdvancementRequirementsClass.getConstructor(String[][].class);
+                return nmsAdvancementClass.getConstructor(Optional.class, Optional.class, nmsAdvancementRewardsClass, Map.class, nmsAdvancementRequirementsClass, boolean.class);
             });
             nmsAdvancementProgressClass = NMSUtils.getNMSClass("net.minecraft.server.%s.AdvancementProgress", "net.minecraft.advancements.AdvancementProgress");
             nmsAdvancementProgressConstructor = nmsAdvancementProgressClass.getConstructor();
-            nmsAdvancementProgressAMethod = nmsAdvancementProgressClass.getMethod("a", Map.class, String[][].class);
+            if (InteractiveChat.version.isNewerOrEqualTo(MCVersion.V1_20_2)) {
+                nmsAdvancementProgressAMethod = nmsAdvancementProgressClass.getMethod("a", nmsAdvancementRequirementsClass);
+            } else {
+                nmsAdvancementProgressAMethod = nmsAdvancementProgressClass.getMethod("a", Map.class, String[][].class);
+            }
             nmsAdvancementProgressGetCriterionProgressMethod = NMSUtils.reflectiveLookup(Method.class, () -> {
                 return nmsAdvancementProgressClass.getMethod("getCriterionProgress", String.class);
             }, () -> {
@@ -105,6 +126,10 @@ public class ToastUtils {
             nmsAdvancementProgressGetCriterionProgressBMethod = nmsAdvancementProgressGetCriterionProgressMethod.getReturnType().getMethod("b");
             nmsPacketPlayOutAdvancementsClass = NMSUtils.getNMSClass("net.minecraft.server.%s.PacketPlayOutAdvancements", "net.minecraft.network.protocol.game.PacketPlayOutAdvancements");
             nmsPacketPlayOutAdvancementsConstructor = nmsPacketPlayOutAdvancementsClass.getConstructor(boolean.class, Collection.class, Set.class, Map.class);
+            if (InteractiveChat.version.isNewerOrEqualTo(MCVersion.V1_20_2)) {
+                nmsAdvancementHolderClass = NMSUtils.getNMSClass("net.minecraft.advancements.AdvancementHolder");
+                nmsAdvancementHolderConstructor = nmsAdvancementHolderClass.getConstructor(nmsMinecraftKeyClass, nmsAdvancementClass);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -120,13 +145,22 @@ public class ToastUtils {
             Object advancementDisplay = nmsAdvancementDisplayConstructor.newInstance(ItemStackUtils.toNMSCopy(icon), componentTitle, componentSubtitle, null, nmsAdvancementFrameTypeEnums[2], true, false, true);
 
             Map<String, Object> advCriteria = new HashMap<>();
-            advCriteria.put("for_free", nmsCriterionConstructor.newInstance(nmsCriterionTriggerImpossibleAConstructor.newInstance()));
+            Object nmsCriterion;
+            if (InteractiveChat.version.isNewerOrEqualTo(MCVersion.V1_20_2)) {
+                nmsCriterion = nmsCriterionConstructor.newInstance(nmsCriterionTriggerImpossibleConstructor.newInstance(), nmsCriterionTriggerImpossibleAConstructor.newInstance());
+            } else {
+                nmsCriterion = nmsCriterionConstructor.newInstance(nmsCriterionTriggerImpossibleAConstructor.newInstance());
+            }
+            advCriteria.put("for_free", nmsCriterion);
             List<String[]> fixedRequirements = new ArrayList<>();
             fixedRequirements.add(new String[] {"for_free"});
-            String[][] advRequirements = Arrays.stream(fixedRequirements.toArray()).toArray(String[][]::new);
+            Object advRequirements = Arrays.stream(fixedRequirements.toArray()).toArray(String[][]::new);
 
             Object saveAdv;
-            if (nmsAdvancementConstructor.getParameterCount() == 6) {
+            if (nmsAdvancementConstructor.getParameterTypes()[0].equals(Optional.class)) {
+                advRequirements = nmsAdvancementRequirementsConstructor.newInstance(advRequirements);
+                saveAdv = nmsAdvancementConstructor.newInstance(Optional.of(minecraftKey), Optional.of(advancementDisplay), advRewards, advCriteria, advRequirements, false);
+            } else if (nmsAdvancementConstructor.getParameterCount() == 6) {
                 saveAdv = nmsAdvancementConstructor.newInstance(minecraftKey, null, advancementDisplay, advRewards, advCriteria, advRequirements);
             } else {
                 saveAdv = nmsAdvancementConstructor.newInstance(minecraftKey, null, advancementDisplay, advRewards, advCriteria, advRequirements, false);
@@ -134,11 +168,22 @@ public class ToastUtils {
 
             Map<Object, Object> prg = new HashMap<>();
             Object advPrg = nmsAdvancementProgressConstructor.newInstance();
-            nmsAdvancementProgressAMethod.invoke(advPrg, advCriteria, advRequirements);
+            if (nmsAdvancementProgressAMethod.getParameterCount() == 1) {
+                nmsAdvancementProgressAMethod.invoke(advPrg, advRequirements);
+            } else {
+                nmsAdvancementProgressAMethod.invoke(advPrg, advCriteria, advRequirements);
+            }
             nmsAdvancementProgressGetCriterionProgressBMethod.invoke(nmsAdvancementProgressGetCriterionProgressMethod.invoke(advPrg, "for_free"));
             prg.put(minecraftKey, advPrg);
 
-            PacketContainer packet1 = PacketContainer.fromPacket(nmsPacketPlayOutAdvancementsConstructor.newInstance(false, Arrays.asList(saveAdv), Collections.emptySet(), prg));
+            Object advancementList;
+            if (InteractiveChat.version.isNewerOrEqualTo(MCVersion.V1_20_2)) {
+                advancementList = Arrays.asList(nmsAdvancementHolderConstructor.newInstance(minecraftKey, saveAdv));
+            } else {
+                advancementList = Arrays.asList(saveAdv);
+            }
+
+            PacketContainer packet1 = PacketContainer.fromPacket(nmsPacketPlayOutAdvancementsConstructor.newInstance(false, advancementList, Collections.emptySet(), prg));
             InteractiveChat.protocolManager.sendServerPacket(pinged, packet1);
 
             Set<Object> rm = new HashSet<>();

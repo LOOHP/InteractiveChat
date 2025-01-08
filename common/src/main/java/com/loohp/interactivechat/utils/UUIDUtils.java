ef5d8dc9863a70f916ded715cdb79dbb9f68ffc3
LOOHP <jamesloohp@gmail.com>
2025-01-08 15:24:11 +0000

    Fixed skull value on versions below 1.20.5

---
+++ common/src/main/java/com/loohp/interactivechat/utils/UUIDUtils.java
@@ -0,0 +1,50 @@
+/*
+ * This file is part of InteractiveChat.
+ *
+ * Copyright (C) 2020 - 2025. LoohpJames <jamesloohp@gmail.com>
+ * Copyright (C) 2020 - 2025. Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.loohp.interactivechat.utils;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class UUIDUtils {
+
+    public static UUID toUUID(int[] array) {
+        return new UUID((long) array[0] << 32 | (long) array[1] & 4294967295L, (long) array[2] << 32 | (long) array[3] & 4294967295L);
+    }
+
+    public static int[] toIntArray(UUID uuid) {
+        long most = uuid.getMostSignificantBits();
+        long least = uuid.getLeastSignificantBits();
+        return new int[] {(int) (most >> 32), (int) most, (int) (least >> 32), (int) least};
+    }
+
+    public static byte[] toByteArray(UUID uuid) {
+        byte[] array = new byte[16];
+        ByteBuffer.wrap(array).order(ByteOrder.BIG_ENDIAN).putLong(uuid.getMostSignificantBits()).putLong(uuid.getLeastSignificantBits());
+        return array;
+    }
+
+    public static UUID fromOfflinePlayerName(String name) {
+        return UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(StandardCharsets.UTF_8));
+    }
+
+}
